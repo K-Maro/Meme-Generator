eccb7bacc8194ba1803c683c1d2d5dd17116fcd2
Marek Brajerski <19328@student.ans-elblag.pl>
2023-05-20 22:33:36 +0200

    First Commit

---
+++ components/Meme.jsx
@@ -0,0 +1,77 @@
+
+import React from 'react'
+
+export default function Meme(){
+
+    function handleClick(event){
+        event.preventDefault();    
+    }
+
+    // const [memeImage, setMemeImage] = React.useState("http://i.imgflip.com/1bij.jpg");
+
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+      });
+
+    const [allMemeImages, setAllMemeImages] = React.useState([])
+
+    React.useEffect(function() {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemeImages(data.data.memes))
+    }, [])
+
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemeImages.length)
+        const url =  allMemeImages[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+    }
+
+    function handleChange(event){
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+
+    return (
+        <main>
+            <form className="form" onSubmit={handleClick}>
+                <input 
+                    type="text"
+                    className="form--input"
+                    placeholder="Top text"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text"
+                    className="form--input"
+                    placeholder="Bottom text"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
+                />
+                <button 
+                    onClick={getMemeImage}
+                    className="form--button">
+                    Get a new meme image
+                </button>
+                
+            </form>
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
+
+        </main>
+    )
+}
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from '../components/Header'
+import Meme from '../components/Meme'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <div className='root-div'>
+      <Header/>
+      <Meme/>
+    </div>
+  )
+}
+
+export default App
